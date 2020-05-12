2af02dfe61b17183d78f00ce0be4705612877d96
deepakshisud <41821039+deepakshisud@users.noreply.github.com>
2020-05-12 18:37:40 +0530

    Add files via upload

---
+++ sys_lock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FirstOS
+{
+	class sys_lock
+	{
+        public static void lockpass(string passcode)
+        {
+            string version = "2019-04-03";
+            bool unlocked = false;
+            while (!unlocked)
+            {
+                Console.Clear();
+                logo.Logo(version);
+                Console.WriteLine("                                                                                ");
+                Console.WriteLine("                                System Locked                                   ");
+                Console.WriteLine("                                                                                ");
+                Console.Write("Password: ");
+                string enterpass = Console.ReadLine();
+                if (enterpass == passcode)
+                {
+                    unlocked = true;
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
